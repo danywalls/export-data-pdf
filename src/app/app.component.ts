a7520810137616bb70d3dfce7667151d36f66250
Dany Paredes <danywalls4@gmail.com>
2023-01-28 13:20:02 +0100

    kendo pdf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { NbaService } from './services/nba.service';
+import { Component, inject } from '@angular/core';
+import { PDFExportComponent } from '@progress/kendo-angular-pdf-export';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'export-data-pdf';
+  nba = inject(NbaService)
+  players$ = this.nba.get();
+  userName!: string;
+
+  getAllBtnStatus = 'Get All Players';
+  remoteData: any;
+
+  async getAll(pdfExport: PDFExportComponent) {
+    this.getAllBtnStatus = "Downloading Data..."
+    const data = await firstValueFrom(this.players$);
+    this.remoteData = data;
+    setTimeout(() => {
+      this.getAllBtnStatus = "Get All Players"
+      pdfExport.saveAs('all-data.pdf');
+    }, 2000)
+  }
 }
